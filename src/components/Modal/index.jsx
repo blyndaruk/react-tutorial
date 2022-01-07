6d0d9a25f8e411352e36be590ae400c790a3b924
Dmitriy Blyndaruk <dmitriy.blyndaruk@gmail.com>
2022-01-07 19:57:29 +0200

    add modal component

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import classNames from 'classnames';
+import Portal from '../Portal/Portal';
+
 import './modal.scss'
 
-const Modal = ({ classes, type, active, setActive, width, height, children }) => {
+const Modal = (
+  {
+    active,
+    children,
+    classes,
+    setActive,
+    type,
+    width,
+    height,
+  }
+) => {
   const modalClasses = classNames('modal', { [`modal-${type}`]: type, 'is-active': active }, classes);
+  const modalRef = React.useRef(null)
+
+  useEffect(() => {
+    if (active) document.body.style.overflow = 'hidden';
+    return () => { document.body.style.overflow = 'unset' }
+  }, [active]);
 
   return (
-    <div className={modalClasses} onClick={() => setActive(false)}>
-      <div className="modal__content" onClick={(e) => e.stopPropagation()} style={{ width, height }}>
-        {children}
-      </div>
-    </div>
+    <CSSTransition
+      in={active}
+      nodeRef={modalRef}
+      classNames="modal"
+      timeout={{
+        appear: 0,
+        enter: 0,
+        exit: 200,
+      }}
+      mountOnEnter
+      unmountOnExit
+    >
+      <Portal className="modal-portal">
+        <div className={modalClasses} onClick={() => setActive(false)} ref={modalRef}>
+          <div className="modal__content" onClick={(e) => e.stopPropagation()} style={{ width, height }}>
+            {children}
+          </div>
+        </div>
+      </Portal>
+    </CSSTransition>
   );
 };
 
