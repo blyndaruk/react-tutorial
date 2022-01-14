53152cb00fcb614c4c8988f1b8159ea4bc19af7f
Dmitriy Blyndaruk <dmitriy.blyndaruk@gmail.com>
2022-01-14 15:42:14 +0200

    feat: usePosts custom hook

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import Posts from './components/Posts';
 import PostsFilter from './components/PostsFilter';
 import Modal from './components/Modal';
 import AddPostForm from './components/AddPostForm';
+import { usePosts } from './hooks/usePosts';
 
 function App () {
   const [posts, setPosts] = useState([
-    { id: 1, title: 'Afasf title', description: 'Best description' },
-    { id: 2, title: 'Ladgadg title2', description: 'Test description' },
-    { id: 3, title: 'Casgas title3', description: 'West description' },
-    { id: 4, title: 'Some title4', description: 'Cest description' },
+    { id: 1, title: 'First title', description: 'Best description' },
+    { id: 2, title: 'Second title2', description: 'Test description' },
+    { id: 3, title: 'Third title3', description: 'West description' },
+    { id: 4, title: 'www title4', description: 'Cest description' },
+    { id: 5, title: 'Aaa title4', description: 'Cest description' },
   ])
 
   const [filter, setFilter] = useState({ sort: '', query: '' })
   const [modalActive, setModalActive] = useState(false)
-
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-    } else {
-      return posts
-    }
-  }, [filter.sort, posts])
-
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter((post) =>
-      post.title.toLowerCase().includes(filter.query.toLowerCase())
-      || post.description.toLowerCase().includes(filter.query.toLowerCase())
-    )
-  }, [filter.query, sortedPosts])
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
   function onSubmit (newPost) {
     setPosts([...posts, newPost])
